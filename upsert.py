c3a9207ed115a865c8ee88c20938dd0e121df69b
Dave MacRae <dave@macrae.org.uk>
2023-04-27 15:37:28 +0100

    Tidied up code.

--- upsert.py
+++ upsert.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python
-
-import boto3
-import sys
-import json
-import socket
-
 '''
     TODO: Compare current A record to MY_IP and only update if necessary
     TODO: Error handling
     DONE: Pass domain or IP as arguments
 '''
 
+import socket
+import re
+import argparse
+import boto3
+
 def domain_check(domain):
-
-    import re
-
+    ''' Check that the format of the domain is correct'''
     pattern = re.compile(
         r'^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|'
         r'([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|'
@@ -25,21 +22,21 @@ def domain_check(domain):
 
 def ip_check(ip_address):
     ''' Check that we have a valid IP address '''
-    import re
-    IP_REXEX = r"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
+    pattern = re.compile(r"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$")
 
-    if re.search(IP_REXEX, ip_address):
-        bytes = ip_address.split(".")
-    
-        for ip_byte in bytes:
+    if pattern.match(ip_address):
+        ip_slice = ip_address.split(".")
+
+        for ip_byte in ip_slice:
             if int(ip_byte) < 0 or int(ip_byte) > 255:
                 return False
-    return True
+        return True
+
+    return False
 
 
 def process_arguments():
-    import argparse
-
+    ''' Process the command line arguments '''
     class IPAction(argparse.Action):
         ''' This will raise an exception if the IP address supplied is not valid '''
         def __call__(self, parser, namespace, values, option_string=None):
@@ -57,57 +54,13 @@ def process_arguments():
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--ip", action=IPAction, help="The IP address you want to use")
-    group.add_argument("--domain", action=DomainAction, help="The domain to retrieve the IP address from")
+    group.add_argument("--domain", action=DomainAction,
+                        help="The domain to retrieve the IP address from")
     parser.add_argument("--verbose", "--v", help="Verbose output", action="store_true")
-    parser.add_argument("--dry-run", help="Don't do anything, just print out proposed actions", action="store_true")
+    parser.add_argument("--dry-run", action="store_true",
+                        help="Don't do anything, just print out proposed actions")
     return parser.parse_args()
 
-def upsert (ip):
-
-    client = boto3.client('route53')
-
-    response = client.get_hosted_zone_count()
-
-    # How many hosted zones do we need to update
-    num_zones = response['HostedZoneCount']
-
-    zones = client.list_hosted_zones()
-    
-    for i in range(num_zones):
-        # The Id string is /hosted/XXXXXXXXXXX but we only need the XXXXXXXXXXX part
-        zone_id = zones['HostedZones'][i]['Id'].split('/')[2]
-        # This gives us the base domain name with a trailing "." which we remove
-        domain = zones['HostedZones'][i]['Name'][:-1]
-
-        changes = {
-            'Changes': [
-                {
-                    'Action': 'UPSERT',
-                    'ResourceRecordSet': {
-                        'Name': domain,
-                        'ResourceRecords': [
-                            {
-                                'Value': ip
-                            }
-                        ],
-                        'TTL': 300,
-                        'Type': 'A',
-                    },
-                },
-            ],
-            'Comment': 'Upserted'
-        }
-
-        # TODO: Make this an option
-        if args.verbose:
-            print (f"Update {domain} to {ip}")
-
-        if args.dry_run:
-            print (f"DRY-RUN: Update {domain} to {ip}")
-        else:
-            upsert_response = client.change_resource_record_sets(ChangeBatch=changes, HostedZoneId=zone_id)
-
-    return
 
 def get_ips_by_dns_lookup(target, port=None):
     '''
@@ -131,7 +84,55 @@ def get_ips_by_dns_lookup(target, port=None):
     if args.verbose:
         print (f"Look up IP address for {target}")
 
-    return list(map(lambda x: x[4][0], socket.getaddrinfo('{}.'.format(target),port,type=socket.SOCK_STREAM)))
+    return list(map(lambda x: x[4][0],
+                    socket.getaddrinfo('{}.'.format(target),port,type=socket.SOCK_STREAM)))
+
+
+def upsert (ip_address):
+    ''' Main Programme '''
+    client = boto3.client('route53')
+
+    response = client.get_hosted_zone_count()
+
+    # How many hosted zones do we need to update
+    num_zones = response['HostedZoneCount']
+
+    zones = client.list_hosted_zones()
+
+    for i in range(num_zones):
+        # The Id string is /hosted/XXXXXXXXXXX but we only need the XXXXXXXXXXX part
+        zone_id = zones['HostedZones'][i]['Id'].split('/')[2]
+        # This gives us the base domain name with a trailing "." which we remove
+        domain = zones['HostedZones'][i]['Name'][:-1]
+
+        changes = {
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': domain,
+                        'ResourceRecords': [
+                            {
+                                'Value': ip_address
+                            }
+                        ],
+                        'TTL': 300,
+                        'Type': 'A',
+                    },
+                },
+            ],
+            'Comment': 'Upserted'
+        }
+
+        if args.verbose:
+            print (f"Update {domain} to {ip}")
+
+        if args.dry_run:
+            print (f"DRY-RUN: Update {domain} to {ip}")
+        else:
+            # TODO: Error handling
+            client.change_resource_record_sets(ChangeBatch=changes, HostedZoneId=zone_id)
+
 
 
 if __name__ == '__main__':
@@ -141,6 +142,10 @@ if __name__ == '__main__':
     if args.ip:
         ip = args.ip
     else:
+        """
+            TODO: We should move this to the domain validator function to check that it
+            is a valid domain
+        """
         ip = get_ips_by_dns_lookup(args.domain)[0]
 
     upsert (ip)
